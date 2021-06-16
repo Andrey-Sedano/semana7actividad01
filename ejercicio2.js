aba1689c6e39afd18bd24fc429bb9099d34829c8
Andrey Sedano <assedano2@misena.edu.co>
2021-06-15 23:53:04 -0500

    ejercicio2

---
+++ ejercicio2.js
@@ -0,0 +1,9 @@
+let n1=parseInt(prompt('ingrese numero'));
+let n2=parseInt(prompt('ingrese numero'));
+
+ resultado=n1/n2; 
+ resultado2=n1%n2;
+alert("el cociente es:"+resultado);
+alert("el residuo es:"+resultado2);
+
+
